d91771a58a1aed7a860c4e78c0e2e27cf8718a45
super <Supergamer66000@gmail.com>
2024-01-18 11:39:53 -0600

    Still has issue in Model at line 17. I think it has something to do with the vertex shader. Or OpenGL_accelerate fix pygame

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import pygame
 import OpenGL
 import moderngl as mgl
 from model import *
+from camera import Camera
 
 class GraphicsEngine:
     __version__ = 0.1
     def __init__(self, win_size=(854, 480)):
         # init pygame and OpenGL
         pygame.init()
+        # Window size
+        self.WIN_SIZE = win_size
         pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
         pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
         pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
         #  create an object to help tack time
         self.clock = pygame.time.Clock()
+        self.time = 0
         # Set Vars
         pygame.display.set_mode(win_size, flags=pygame.OPENGL | pygame.DOUBLEBUF)  # Set window size and Initializes OpenGL for pygame
         # Detect and use existing OpenGL content
         self.ctx = mgl.create_context()
+        # Camera
+        self.camera = Camera(self)
         # scene
-        self.scene = Triangle(self)
+        self.scene = Cube(self)
 
     def check_events(self):
         for event in pygame.event.get():
@@ -35,8 +41,12 @@ class GraphicsEngine:
         self.scene.render()
         # Swaps Buffers
         pygame.display.flip()
+
+    def get_time(self):
+        self.time = pygame.time.get_tick() * 0.001
     def run(self):
         while True:
+            self.get_time()
             self.check_events()
             self.render()
             self.clock.tick(60)
--- model.py
+++ model.py
@@ -1,26 +1,53 @@
-import OpenGL.GL
 import numpy as np
+import glm
 
-class Triangle:
+class Cube:
     def __init__(self, app):
         self.app = app
         self.ctx = app.ctx
         self.vbo = self.get_vbo()
         self.shader_program = self.get_shader_program('default')
         self.vao = self.get_vao()
+        self.m_model = self.get_model_matrix()
+        self.on_init()
+    def get_model_matrix(self):
+        m_model = glm.mat4()
+        return m_model
+    def on_init(self):
+        # Passes it into input
+        self.shader_program['m_proj'].write(self.app.camera.m_proj)
+        self.shader_program['m_view'].write(self.app.camera.m_view)
+        self.shader_program['m_model'].write(self.m_model)
+    def update(self):
+        m_model = glm.rotate(self.m_model, self.app.time, glm.vec3(0, 1, 0))
+        self.shader_program['m_model'].write(self.m_model)
     def render(self):
+        self.update()
         self.vao.render()
     def destroy(self):
         self.vbo.release()
         self.shader_program.release()
         self.vao.release()
     def get_vao(self):
-        vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, 'f3', 'in_position')])
+        vao = self.ctx.vertex_array(self.shader_program, [(self.vbo, '3f', 'in_position')])
         return vao
     def get_vertex_data(self):
-        vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
-        vertex_data = np.array(vertex_data, dtype='f4')
+        vertices = [(-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1),
+                    (-1, 1, -1), (-1, -1, -1), (1, -1, -1), (1, 1, -1)]
+        indices = [(0, 2, 3), (0, 1, 2),
+                   (1, 7, 2), (1, 6, 7),
+                   (6, 5, 4), (4, 7, 6),
+                   (3, 4, 5), (3, 5, 0),
+                   (3, 7, 4), (3, 2, 7),
+                   (0, 6, 1), (0, 5, 6)]
+
+        vertex_data = self.get_data(vertices, indices)
         return vertex_data
+
+    @staticmethod
+    def get_data(vertices, indices):
+        data = [vertices[ind] for triangle in indices for ind in triangle]
+        return np.array(data, dtype='f4')
     def get_vbo(self):
         vertex_data = self.get_vertex_data()
         vbo = self.ctx.buffer(vertex_data.astype('f4'))
